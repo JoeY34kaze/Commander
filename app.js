181540815bda44f5217a3acb3b6bb159e2112974
mc0239 <mc0239@student.uni-lj.si>
2017-11-15 20:35:53 +0100

    updated gitignore; added eslintrc file; fixed indentation

--- app.js
+++ app.js
@@ -2,17 +2,18 @@ var gl_global;
 var canvas_global;
 var camera;
 
-var environment=new Array; //not se loh doda z global.environment.push(obj);
+var environment = []; //not se loh doda z global.environment.push(obj);
 
 
 //var vs_global;
 //var fs_global;
+var kek;
 var program_global;
 var boxIndices_global;
 
-var instantiate_cube=function(){
+var instantiate_cube = function() {
 	window.canvas_global = document.getElementById('game-surface');
-	gl = window.canvas_global.getContext('webgl');
+	let gl = window.canvas_global.getContext('webgl');
 	if (!gl) {
 		console.log('WebGL not supported, falling back on experimental-webgl');
 		gl = window.canvas_global.getContext('experimental-webgl');
@@ -30,151 +31,146 @@ var instantiate_cube=function(){
 	gl.cullFace(gl.BACK);
 
 
-		//
-		// Create shaders
-		// 
-		var fragmentShader = getShader(gl, "shader-fs");
-		var vertexShader = getShader(gl, "shader-vs");
+	//
+	// Create shaders
+	// 
+	var fragmentShader = getShader(gl, "shader-fs");
+	var vertexShader = getShader(gl, "shader-vs");
 
-	  gl.compileShader(vertexShader);
-	  if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
-		  console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
-		  return;
-	  }
+	gl.compileShader(vertexShader);
+	if (!gl.getShaderParameter(vertexShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling vertex shader!', gl.getShaderInfoLog(vertexShader));
+		return;
+	}
   
-	  gl.compileShader(fragmentShader);
-	  if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
-		  console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
-		  return;
-	  }
+	gl.compileShader(fragmentShader);
+	if (!gl.getShaderParameter(fragmentShader, gl.COMPILE_STATUS)) {
+		console.error('ERROR compiling fragment shader!', gl.getShaderInfoLog(fragmentShader));
+		return;
+	}
 
-	  program = gl.createProgram();
-	  gl.attachShader(program, vertexShader);
-	  gl.attachShader(program, fragmentShader);
-	  gl.linkProgram(program);
-	  if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-		  console.error('ERROR linking program!', gl.getProgramInfoLog(program));
-		  return;
-	  }
-	  gl.validateProgram(program);
-	  if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
-		  console.error('ERROR validating program!', gl.getProgramInfoLog(program));
-		  return;
-	  }
-  
-	  //
-	  // Create buffer
-	  //
-	  var boxVertices = 
-	  [ // X, Y, Z           R, G, B
-		  // Top
-		  -1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
-		  -1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
-		  1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
-		  1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
-  
-		  // Left
-		  -1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
-		  -1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
-		  -1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
-		  -1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
-  
-		  // Right
-		  1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
-		  1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
-		  1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
-		  1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
-  
-		  // Front
-		  1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-		  1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		  -1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		  -1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-  
-		  // Back
-		  1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-		  1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		  -1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		  -1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-  
-		  // Bottom
-		  -1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
-		  -1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
-		  1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
-		  1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
-	  ];
-  
-	  var boxIndices =
-	  [
-		  // Top
-		  0, 1, 2,
-		  0, 2, 3,
-  
-		  // Left
-		  5, 4, 6,
-		  6, 4, 7,
-  
-		  // Right
-		  8, 9, 10,
-		  8, 10, 11,
-  
-		  // Front
-		  13, 12, 14,
-		  15, 14, 12,
-  
-		  // Back
-		  16, 17, 18,
-		  16, 18, 19,
-  
-		  // Bottom
-		  21, 20, 22,
-		  22, 20, 23
-	  ];
-  
-	  var boxVertexBufferObject = gl.createBuffer();
-	  gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-	  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-  
-	  var boxIndexBufferObject = gl.createBuffer();
-	  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-	  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
-  
-	  var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
-	  var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
-	  gl.vertexAttribPointer(
-		  positionAttribLocation, // Attribute location
-		  3, // Number of elements per attribute
-		  gl.FLOAT, // Type of elements
-		  gl.FALSE,
-		  6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		  0 // Offset from the beginning of a single vertex to this attribute
-	  );
-	  gl.vertexAttribPointer(
-		  colorAttribLocation, // Attribute location
-		  3, // Number of elements per attribute
-		  gl.FLOAT, // Type of elements
-		  gl.FALSE,
-		  6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
-		  3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
-	  );
-  
-	  gl.enableVertexAttribArray(positionAttribLocation);
-	  gl.enableVertexAttribArray(colorAttribLocation);
+	let program = gl.createProgram();
+	gl.attachShader(program, vertexShader);
+	gl.attachShader(program, fragmentShader);
+	gl.linkProgram(program);
+	if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+		console.error('ERROR linking program!', gl.getProgramInfoLog(program));
+		return;
+	}
+	gl.validateProgram(program);
+	if (!gl.getProgramParameter(program, gl.VALIDATE_STATUS)) {
+		console.error('ERROR validating program!', gl.getProgramInfoLog(program));
+		return;
+	}
 
+	//
+	// Create buffer
+	//
+	var boxVertices = 
+	[ // X, Y, Z           R, G, B
+		// Top
+		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
 
+		// Left
+		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+		// Right
+		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+		// Front
+		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+		// Back
+		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+		// Bottom
+		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
+	];
+  
+	var boxIndices =
+	[
+		// Top
+		0, 1, 2,
+		0, 2, 3,
+
+		// Left
+		5, 4, 6,
+		6, 4, 7,
+
+		// Right
+		8, 9, 10,
+		8, 10, 11,
+
+		// Front
+		13, 12, 14,
+		15, 14, 12,
+
+		// Back
+		16, 17, 18,
+		16, 18, 19,
+
+		// Bottom
+		21, 20, 22,
+		22, 20, 23
+	];
+  
+	var boxVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
+  
+	var boxIndexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
+  
+	var positionAttribLocation = gl.getAttribLocation(program, 'vertPosition');
+	var colorAttribLocation = gl.getAttribLocation(program, 'vertColor');
+	gl.vertexAttribPointer(
+		positionAttribLocation, // Attribute location
+		3, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		0 // Offset from the beginning of a single vertex to this attribute
+	);
+	gl.vertexAttribPointer(
+		colorAttribLocation, // Attribute location
+		3, // Number of elements per attribute
+		gl.FLOAT, // Type of elements
+		gl.FALSE,
+		6 * Float32Array.BYTES_PER_ELEMENT, // Size of an individual vertex
+		3 * Float32Array.BYTES_PER_ELEMENT // Offset from the beginning of a single vertex to this attribute
+	);
+  
+	gl.enableVertexAttribArray(positionAttribLocation);
+	gl.enableVertexAttribArray(colorAttribLocation);
 
 
-
-
-
-	window.gl_global=gl;
+	window.gl_global = gl;
 	//window.vs_global=vertexShader;
 	//window.fs_global=fragmentShader;
 	//window.program_global=program;
 	//window.boxIndices_global=boxIndices;
 
 
-	var platform_cube={
+	var platform_cube = {
 		program:program,
 		boxIndices:boxIndices,
 	};
@@ -182,20 +178,20 @@ var instantiate_cube=function(){
 	window.environment.push(platform_cube);
 
 
-}
-var initialize_camera=function(){
-	window.camera={
+};
+var initialize_camera = function() {
+	window.camera = {
 		position:[0, 0.5, -6]
 	};
-}
+};
 
-var init=function(){
+var init = function() {
 	initialize_camera();
 	instantiate_cube();
-}
+};
 
 
-var drawCube=function(positionV,rotationV,angle,scaleV, program, boxIndices){
+var drawCube = function(positionV,rotationV,angle,scaleV, program, boxIndices) {
 	/*what dis do:
 	pos-pozicija(ubistvu se kamera odmika)
 	rot-vektor rotacije([1,0,0]) je okol x recimo
@@ -205,7 +201,7 @@ var drawCube=function(positionV,rotationV,angle,scaleV, program, boxIndices){
 	*/
 	
 	//console.log('This is working');
-		var gl=window.gl_global;
+		var gl = window.gl_global;
 		//var fragmentShader=window.fs_global;
 		//var vertexShader=window.vs_global;
 		//var program=window.program_global;
@@ -236,7 +232,7 @@ var drawCube=function(positionV,rotationV,angle,scaleV, program, boxIndices){
 		//------------------------------------TRANSFORMACIJE--------------------------------------------------
 		//tipicno je TRS - translacija, rotacija, skaliranje
 
-		mat4.scale(scaleMatrix,identityMatrix,scaleV)
+		mat4.scale(scaleMatrix,identityMatrix,scaleV);
 		console.log(worldMatrix);
 		mat4.mul(worldMatrix,worldMatrix,scaleMatrix);
 		console.log(worldMatrix);
@@ -248,7 +244,7 @@ var drawCube=function(positionV,rotationV,angle,scaleV, program, boxIndices){
 		gl.clearColor(0.75, 0.85, 0.8, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 		gl.drawElements(gl.TRIANGLES, boxIndices.length, gl.UNSIGNED_SHORT, 0);
-}
+};
 
 //
 // getShader
@@ -263,7 +259,7 @@ function getShader(gl, id) {
 
   // Didn't find an element with the specified ID; abort.
   if (!shaderScript) {
-    return null;
+	return null;
   }
 
   // Walk through the source element's children, building the
@@ -271,21 +267,21 @@ function getShader(gl, id) {
   var shaderSource = "";
   var currentChild = shaderScript.firstChild;
   while (currentChild) {
-    if (currentChild.nodeType == 3) {
-        shaderSource += currentChild.textContent;
-    }
-    currentChild = currentChild.nextSibling;
+	if (currentChild.nodeType == 3) {
+		shaderSource += currentChild.textContent;
+	}
+	currentChild = currentChild.nextSibling;
   }
   
   // Now figure out what type of shader script we have,
   // based on its MIME type.
   var shader;
   if (shaderScript.type == "x-shader/x-fragment") {
-    shader = gl.createShader(gl.FRAGMENT_SHADER);
+	shader = gl.createShader(gl.FRAGMENT_SHADER);
   } else if (shaderScript.type == "x-shader/x-vertex") {
-    shader = gl.createShader(gl.VERTEX_SHADER);
+	shader = gl.createShader(gl.VERTEX_SHADER);
   } else {
-    return null;  // Unknown shader type
+	return null;  // Unknown shader type
   }
 
   // Send the source to the shader object
@@ -296,36 +292,23 @@ function getShader(gl, id) {
 
   // See if it compiled successfully
   if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
-    alert(gl.getShaderInfoLog(shader));
-    return null;
+	alert(gl.getShaderInfoLog(shader));
+	return null;
   }
 
-  return shader;
+  return shader;   
 }
-
-
-
 
 var onStart = function () {
 
-	init(); // tle naj se zgodi vsa inicializacija objektov, karkoli se bo dlje časa rabilo met.
-
-
-	
-	
-	
+	init(); // tle naj se zgodi vsa inicializacija objektov, karkoli se bo dlje časa rabilo met.	
 	
 	gameplay();//tle not djmo stlacit logiko igranja
-	
-	
-	
-	
 	
 	//one loop to rule them all, one loop to draw them, one loop to transform them all and in the renderer bind them
 	var loop = function () {//loop ki transformira vse objekte in jih izrise
 
-
-		window.environment.forEach(function(object){
+		window.environment.forEach(function(object) {
 			//console.log(window.environment);
 
 			drawCube([0,-2,0],[0,0,0],0,[2,1,1],object.program,object.boxIndices);  //positionV,rotationV,angle,scaleV, dva parametra ki sta nujna za webgl engine
@@ -341,6 +324,6 @@ var onStart = function () {
 };
 
 
-var gameplay=function(){//do stuff
+var gameplay = function() {//do stuff
 
-}
+};
