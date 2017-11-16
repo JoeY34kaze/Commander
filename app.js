4a3dea590a3d80f151fa8e8cc45d414dc110b630
mc0239 <mc0239@student.uni-lj.si>
2017-11-16 14:04:28 +0100

    Generalization of createObject and draw functions

--- app.js
+++ app.js
@@ -1,171 +1,3 @@
-var environment = []; //not se loh doda z global.environment.push(obj);
-
-var instantiateCube = function(position = [0, 0, 0], rotation = [0, 0, 0], scale = [1, 1, 1]) {
-
-	//
-	// Create buffer
-	//
-	// X, Y, Z           R, G, B
-	let boxVertices = [ 
-		// Top
-		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
-		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
-		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
-		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
-
-		// Left
-		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
-		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
-		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
-		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
-
-		// Right
-		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
-		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
-		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
-		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
-
-		// Front
-		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
-		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
-
-		// Back
-		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
-		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
-
-		// Bottom
-		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
-		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
-		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
-		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
-	];
-  
-	let boxIndices = [
-		// Top
-		0, 1, 2,
-		0, 2, 3,
-
-		// Left
-		5, 4, 6,
-		6, 4, 7,
-
-		// Right
-		8, 9, 10,
-		8, 10, 11,
-
-		// Front
-		13, 12, 14,
-		15, 14, 12,
-
-		// Back
-		16, 17, 18,
-		16, 18, 19,
-
-		// Bottom
-		21, 20, 22,
-		22, 20, 23
-	];
-  
-	let boxVertexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(boxVertices), gl.STATIC_DRAW);
-  
-	let boxIndexBufferObject = gl.createBuffer();
-	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
-	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(boxIndices), gl.STATIC_DRAW);
-
-	let shaderProgram = createShaderProgram();
-  
-	let positionAttribLocation = gl.getAttribLocation(shaderProgram, 'vertPosition');
-	let colorAttribLocation = gl.getAttribLocation(shaderProgram, 'vertColor');
- 
-	// gl.vertexAttribPointer(
-	//   Attribute location, 
-	//   Number of elements per attribute, 
-	//   Type of elements, 
-	//   , 
-	//   Size of an individual vertex, 
-	//   Offset from the beginning of a single vertex to this attribute
-	// );
-	gl.vertexAttribPointer(positionAttribLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
-	gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
-  
-	gl.enableVertexAttribArray(positionAttribLocation);
-	gl.enableVertexAttribArray(colorAttribLocation);
-
-	var object = {
-		program: shaderProgram,
-		indices: boxIndices,
-		position: position,
-		rotation: rotation,
-		angle: 0,
-		scale: scale
-	};
-
-	environment.push(object);
-};
-
-var init = function() {
-	// init camera
-	camera = {
-		position:[0, 0.5, -6]
-	};
-	instantiateCube([0, -1, 0]);
-	instantiateCube([0, -2, 0], undefined, [2, 1, 1]);
-	console.log(environment);
-};
-
-/*what dis do:
-	pos-pozicija(ubistvu se kamera odmika)
-	rot-vektor rotacije([1,0,0]) je okol x recimo
-	angle - kot okol prejsnga vektorja
-	scaleV - vektor for skeil [sx,sy,sz]
-
-	*/
-var draw = function(object) {
-	let program = object.program;
-
-	gl.useProgram(program);
-
-	var matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
-	var matViewUniformLocation = gl.getUniformLocation(program, 'mView');
-	var matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
-
-	var worldMatrix = new Float32Array(16);
-	var viewMatrix = new Float32Array(16);
-	var projMatrix = new Float32Array(16);
-	mat4.identity(worldMatrix);
-	mat4.lookAt(viewMatrix, camera.position, [0,0,0], [0, 1, 0]); //camera (pozicija kamere, kam gleda , vektor ki kaze gor)
-	mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
-
-	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
-	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
-
-	var rotationMatrix 	= new Float32Array(16);
-	var scaleMatrix		= new Float32Array(16);
-	var translation		= new Float32Array(16);
-	var identityMatrix = new Float32Array(16);
-	mat4.identity(identityMatrix);
-	//------------------------------------TRANSFORMACIJE--------------------------------------------------
-	//tipicno je TRS - translacija, rotacija, skaliranje
-
-	mat4.scale(scaleMatrix,identityMatrix,object.scale);
-	//console.log(worldMatrix);
-	mat4.mul(worldMatrix,worldMatrix,scaleMatrix);
-	//console.log(worldMatrix);
-	//mat4.rotate(rotationMatrix,identityMatrix,angle,rotationV);
-	//mat4.mul(worldMatrix, worldMatrix, rotationMatrix);
-	mat4.translate(worldMatrix, worldMatrix,object.position);
-
-	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
-	gl.drawElements(gl.TRIANGLES, object.indices.length, gl.UNSIGNED_SHORT, 0);
-};
-
 var canvas;
 var gl;
 
@@ -173,6 +5,8 @@ var vertexShader;
 var fragmentShader;
 
 var camera;
+
+var environment = []; //not se loh doda z global.environment.push(obj);
 
 var onStart = function () {
 
@@ -208,6 +42,165 @@ var onStart = function () {
 	requestAnimationFrame(update);
 
 };
+
+// keira objekt s podanimi parametri (obvezno podati vertice in indice)
+function createObject(vertices, indices, position = [0, 0, 0], rotation = [0, 0, 0], scale = [1, 1, 1]) {
+	// Create buffers for object
+	let boxVertexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ARRAY_BUFFER, boxVertexBufferObject);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  
+	let boxIndexBufferObject = gl.createBuffer();
+	gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, boxIndexBufferObject);
+	gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(indices), gl.STATIC_DRAW);
+
+	let shaderProgram = createShaderProgram();
+  
+	let positionAttribLocation = gl.getAttribLocation(shaderProgram, 'vertPosition');
+	let colorAttribLocation = gl.getAttribLocation(shaderProgram, 'vertColor');
+ 
+	// gl.vertexAttribPointer(
+	//   Attribute location, 
+	//   Number of elements per attribute, 
+	//   Type of elements, 
+	//   , 
+	//   Size of an individual vertex, 
+	//   Offset from the beginning of a single vertex to this attribute
+	// );
+	gl.vertexAttribPointer(positionAttribLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 0);
+	gl.vertexAttribPointer(colorAttribLocation, 3, gl.FLOAT, gl.FALSE, 6 * Float32Array.BYTES_PER_ELEMENT, 3 * Float32Array.BYTES_PER_ELEMENT);
+  
+	gl.enableVertexAttribArray(positionAttribLocation);
+	gl.enableVertexAttribArray(colorAttribLocation);
+
+	var object = {
+		program: shaderProgram,
+		indices: indices,
+		position: position,
+		rotation: rotation,
+		angle: 0,
+		scale: scale
+	};
+
+	environment.push(object);
+}
+
+var objectsVI = {
+	// X, Y, Z           R, G, B
+	boxVertices: [ 
+		// Top
+		-1.0, 1.0, -1.0,   0.5, 0.5, 0.5,
+		-1.0, 1.0, 1.0,    0.5, 0.5, 0.5,
+		1.0, 1.0, 1.0,     0.5, 0.5, 0.5,
+		1.0, 1.0, -1.0,    0.5, 0.5, 0.5,
+
+		// Left
+		-1.0, 1.0, 1.0,    0.75, 0.25, 0.5,
+		-1.0, -1.0, 1.0,   0.75, 0.25, 0.5,
+		-1.0, -1.0, -1.0,  0.75, 0.25, 0.5,
+		-1.0, 1.0, -1.0,   0.75, 0.25, 0.5,
+
+		// Right
+		1.0, 1.0, 1.0,    0.25, 0.25, 0.75,
+		1.0, -1.0, 1.0,   0.25, 0.25, 0.75,
+		1.0, -1.0, -1.0,  0.25, 0.25, 0.75,
+		1.0, 1.0, -1.0,   0.25, 0.25, 0.75,
+
+		// Front
+		1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+		1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, -1.0, 1.0,    1.0, 0.0, 0.15,
+		-1.0, 1.0, 1.0,    1.0, 0.0, 0.15,
+
+		// Back
+		1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+		1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, -1.0, -1.0,    0.0, 1.0, 0.15,
+		-1.0, 1.0, -1.0,    0.0, 1.0, 0.15,
+
+		// Bottom
+		-1.0, -1.0, -1.0,   0.5, 0.5, 1.0,
+		-1.0, -1.0, 1.0,    0.5, 0.5, 1.0,
+		1.0, -1.0, 1.0,     0.5, 0.5, 1.0,
+		1.0, -1.0, -1.0,    0.5, 0.5, 1.0,
+	],
+	boxIndices: [
+		// Top
+		0, 1, 2,
+		0, 2, 3,
+
+		// Left
+		5, 4, 6,
+		6, 4, 7,
+
+		// Right
+		8, 9, 10,
+		8, 10, 11,
+
+		// Front
+		13, 12, 14,
+		15, 14, 12,
+
+		// Back
+		16, 17, 18,
+		16, 18, 19,
+
+		// Bottom
+		21, 20, 22,
+		22, 20, 23
+	]
+};
+
+var init = function() {
+	// init camera
+	camera = {
+		position:[0, 0.5, -6]
+	};
+	createObject(objectsVI.boxVertices, objectsVI.boxIndices, [0, -1, 0]);
+	createObject(objectsVI.boxVertices, objectsVI.boxIndices, [0, -2.5, 0], undefined, [2, 1, 1]);
+	console.log(environment);
+};
+
+// izrise izbran objekt
+var draw = function(object) {
+	let program = object.program;
+
+	gl.useProgram(program);
+
+	let matWorldUniformLocation = gl.getUniformLocation(program, 'mWorld');
+	let matViewUniformLocation = gl.getUniformLocation(program, 'mView');
+	let matProjUniformLocation = gl.getUniformLocation(program, 'mProj');
+
+	let worldMatrix = new Float32Array(16);
+	let viewMatrix = new Float32Array(16);
+	let projMatrix = new Float32Array(16);
+
+	mat4.identity(worldMatrix);
+	mat4.lookAt(viewMatrix, camera.position, [0,0,0], [0, 1, 0]); //camera (pozicija kamere, kam gleda , vektor ki kaze gor)
+	mat4.perspective(projMatrix, glMatrix.toRadian(45), canvas.width / canvas.height, 0.1, 1000.0);
+
+	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+	gl.uniformMatrix4fv(matViewUniformLocation, gl.FALSE, viewMatrix);
+	gl.uniformMatrix4fv(matProjUniformLocation, gl.FALSE, projMatrix);
+
+	//------------------------------------TRANSFORMACIJE--------------------------------------------------
+	//tipicno je TRS - translacija, rotacija, skaliranje
+	let transformMatrix = new Float32Array(16);
+	mat4.identity(transformMatrix);
+
+	mat4.translate(transformMatrix, transformMatrix, object.position);
+	mat4.scale(transformMatrix, transformMatrix, object.scale);
+	// TODO: implementiraj se rotacije ??? mogoce je potrebno drugace podati rotacije kot trenutno
+	//mat4.rotate(transformMatrix, ???);
+
+	mat4.mul(worldMatrix, worldMatrix, transformMatrix);
+
+	gl.uniformMatrix4fv(matWorldUniformLocation, gl.FALSE, worldMatrix);
+	gl.drawElements(gl.TRIANGLES, object.indices.length, gl.UNSIGNED_SHORT, 0);
+};
+
+
+
 
 var gameplay = function() {//do stuff
 
